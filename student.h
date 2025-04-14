d93960e3f0e5715c173c71c6ef6a43be274e0545
Ovchinnikov Anton <157784472+AntonITW@users.noreply.github.com>
2025-04-14 07:57:21 +0300

    Update student.h

--- student.h
+++ student.h
@@ -4,23 +4,10 @@
 
 using namespace std;
 
-class Student {
-public:
+struct Student {
 	string last_name;
 	string first_name;
 	string fathers_name;
 	int age;
 	string gender;
-	Student() = default;
-
-	Student(string plast_name, string pfirst_name, string pfathers_name, int page, string pgender) {
-		last_name = plast_name;
-		first_name = pfirst_name;
-		fathers_name = pfathers_name;
-		age = page;
-		gender = pgender;
-	}
-	void PrintInfo() {
-		cout << "Your data : " << last_name << " " << first_name << " " << fathers_name << " " << age << " " << gender << '\n';
-	}
 };
